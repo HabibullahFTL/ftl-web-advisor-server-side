25855b50625b5e55adb9834ba372902d88bc8e14
Habibullah Bahar Piash <habibullahftl@gmail.com>
2021-04-16 15:20:45 +0600

    started working with server

---
+++ index.js
@@ -0,0 +1,14 @@
+const port = 3005;
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+
+
+app.get("/",(req,res)=>{
+    res.send({name:"Hkk"})
+})
+
+app.listen(port)
