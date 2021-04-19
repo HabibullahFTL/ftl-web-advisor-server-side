5cd047add826ba815db77085708ccf3e5d609059
Habibullah Bahar Piash <habibullahftl@gmail.com>
2021-04-19 10:24:02 +0600

    some functionality modified

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.use(cors())
 const now = moment().format("YYYY-MM-DD HH:mm:ss");
 
 // Main Part Started here
-app.get("/",(req,res)=>{
-    res.send({message:"Welcome to FTL Web Advisor API"})
+app.get("/", (req, res) => {
+    res.send({ message: "Welcome to FTL Web Advisor API" })
 })
 
 // Started integration with mongodb
@@ -28,27 +28,106 @@ client.connect(err => {
 
     if (err) {
         console.log("there is an error");
-    }else{
-       // ============ [ For Creating new service ]==============
-        app.post('/create-service',(req,res)=>{
-            serviceCollection.insertOne(req.body).then(result=>{
-                if(result.insertedCount > 0){
+    } else {
+        // ============ [ For Creating new service ]==============
+        app.post('/create-service', (req, res) => {
+            serviceCollection.insertOne(req.body).then(result => {
+                if (result.insertedCount > 0) {
                     res.send(result.ops[0])
-                }else{
-                    res.send({message:"Something went wrong"})
+                } else {
+                    res.send({ message: "Something went wrong" })
                 }
             })
-        }) 
-  
-        // ============ [ For Showing All services ]==============
-        app.get('/all-services',(req,res)=>{
-            serviceCollection.find({})
-          .sort({_id:-1})
-          .toArray((err,documents)=>{
-              res.send(documents)
-          })
         })
-    }
-  });
 
-app.listen( process.env.PORT || port)
+        // ============ [ For Showing All services ]==============
+        app.get('/all-services', (req, res) => {
+            serviceCollection.find({})
+                .sort({ _id: -1 })
+                .toArray((err, documents) => {
+                    res.send(documents)
+                })
+        })
+
+        // ============ [ For updating service ]==============
+        app.patch('/update-service', (req, res) => {
+            const { service_id } = req.query;
+            const data = req.body;
+            serviceCollection.updateOne(
+                { _id: ObjectID(service_id) },
+                {"set":{data}}
+                )
+                .then(result => {
+                    console.log(result);
+                    res.send(result.deletedCount > 0)
+                })
+                .catch(err => {
+                    res.send(false)
+                })
+        })
+
+        // ============ [ For deleting service ]==============
+        app.delete('/delete-service', (req, res) => {
+            const { service_id } = req.query;
+            serviceCollection.deleteOne({ _id: ObjectID(service_id) })
+                .then(result => {
+                    res.send(result.deletedCount > 0)
+                })
+                .catch(err => {
+                    res.send(false)
+                })
+        })
+
+        // ============ [ For placing new order ]==============
+        app.post('/place-order', (req, res) => {
+            orderCollection.insertOne(req.body).then(result => {
+                if (result.insertedCount > 0) {
+                    res.send(result.ops[0])
+                } else {
+                    res.send({ message: "Something went wrong" })
+                }
+            })
+        })
+
+        // ============ [ For Showing All orders ]==============
+        app.get('/all-orders', (req, res) => {
+            orderCollection.find({})
+                .sort({ _id: -1 })
+                .toArray((err, documents) => {
+                    res.send(documents)
+                })
+        })
+
+        // ============ [ For updating order ]==============
+        app.patch('/update-order', (req, res) => {
+            const { order_id } = req.query;
+            const data = req.body;
+            orderCollection.updateOne(
+                { _id: ObjectID(order_id) },
+                {"set":{data}}
+                )
+                .then(result => {
+                    console.log(result);
+                    res.send(result.deletedCount > 0)
+                })
+                .catch(err => {
+                    res.send(false)
+                })
+        })
+
+        // ============ [ For deleting order ]==============
+        app.delete('/delete-order', (req, res) => {
+            const { order_id } = req.query;
+            orderCollection.deleteOne({ _id: ObjectID(order_id) })
+                .then(result => {
+                    res.send(result.deletedCount > 0)
+                })
+                .catch(err => {
+                    res.send(false)
+                })
+        })
+
+    }
+});
+
+app.listen(process.env.PORT || port)
